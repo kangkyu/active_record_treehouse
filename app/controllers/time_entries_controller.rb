47f4c2601ea31b18dee30e0f7bb4a97c3cac4cef
kangkyu <kangkyu1111@gmail.com>
2015-04-03 20:23:00 -0700

    use log_time model method to time_entries_controller

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,9 +1,9 @@
 class TimeEntriesController < ApplicationController
   def create
     employee = Employee.find(params[:employee_id])
-    @time_entry = employee.time_entries.create(params.require(:time_entry).permit(:customer_id, :time))
-    @customer = Customer.find(params[:time_entry][:customer_id])
+    employee.log_time(params.require(:time_entry).permit(:customer_id, :time))
     redirect_to employee
+    # @customer = Customer.find(params[:time_entry][:customer_id])
     # redirect_to @customer
 
     # "time_entry"=>{"customer_id"=>"3",
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,8 @@
 class Employee < Account
   has_many :time_entries
+
+
+  def log_time(time_entry_params)
+    TimeEntry.create(time_entry_params.merge({employee:self}))
+  end
 end
